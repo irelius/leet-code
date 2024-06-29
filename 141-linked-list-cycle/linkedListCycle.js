177cc80be6c1e68d9ada2abe2e3b7af765ce8167
irelius <kihong711@hotmail.com>
2024-06-29 14:31:31 -0700

    edited to fix bug

--- 141-linked-list-cycle/linkedListCycle.js
+++ 141-linked-list-cycle/linkedListCycle.js
@@ -3,20 +3,12 @@
 import { convertLinkedListToArray, makeLinkedList } from "../helper/linkedList";
 
 const hasCycle = function (head) {
-    if (head === null) {
-        return false
+    let walk = head
+    let fast = head;
+    while (fast && fast.next) {
+        walk = walk.next;
+        fast = fast.next.next;
+        if (walk === fast) return true;
     }
-
-    let pointer1 = head
-    let pointer2 = head
-
-    while (pointer1.next !== null && pointer2.next.next !== null) {
-        if (pointer1.val === pointer2.val) {
-            return true
-        } else {
-            pointer1 = pointer1.next
-            pointer2 = pointer2.next.next
-        }
-    }
-    return false
+    return false;
 };
