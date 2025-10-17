9ae6ccf22a121ff129c8c669d9e055da22ef9a2e
irelius <kihong711@hotmail.com>
2025-10-17 15:58:20 -0700

    update test file for 238

--- tests/238-product-of-array-except-self.test.js
+++ tests/238-product-of-array-except-self.test.js
@@ -6,7 +6,7 @@ describe("238 - Product Of Array Except Self", () => {
 		expect(productOfArrayExceptSelf([1, 2, 4, 6])).toEqual([48, 24, 12, 8]);
 	});
 
-	// test("Case 2", () => {
-	// 	expect(productOfArrayExceptSelf([-1, 0, 1, 2, 3])).toEqual([0, -6, 0, 0, 0]);
-	// });
+	test("Case 2", () => {
+		expect(productOfArrayExceptSelf([-1, 0, 1, 2, 3])).toEqual([0, -6, 0, 0, 0]);
+	});
 });
