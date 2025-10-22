4a29123c72ccd0691646c59955afc243a75502b3
irelius <kihong711@hotmail.com>
2025-10-21 19:11:25 -0700

    15, 3 sum done

--- questions/15-3-sum.js
+++ questions/15-3-sum.js
@@ -1,63 +1,94 @@
-// // Not good solution, takes too long, but theoretically works?
-// export const threeSum = function (nums) {
-//     nums.sort((a, b) => a - b)
-//     let triplets = []
-//     let ref = new Set()
+// https://leetcode.com/problems/3sum/description/
+export const threeSum = (nums) => {
+	// sort the `nums` array from least to greatest
+	nums.sort((a, b) => a - b);
 
-//     let holder = 0
-//     let left = 1
-//     let right = 2
+	// Array to hold the unique combinations of 3 numbers that add to 0
+	const ans = [];
 
-//     while (right <= nums.length) {
-//         let h = nums[holder]
-//         let l = nums[left]
-//         let r = nums[right]
+	// Loop through nums array
+	for (let i = 0; i < nums.length; i++) {
+		// Set `curr` to the current number
+		const curr = nums[i];
 
-//         let rLimit = nums.length - 1
-//         let lLimit = rLimit - 1
+		// If `curr` is greater than 0, quit the loop
+		//      Because the array is sorted from least to greatest, once `curr` hits 0, you can't add to 0 with the numbers past `i`
+		//      All numbers past index `i` will be positive
+		if (curr > 0) break;
 
-//         if (h + l + r === 0) {
-//             let newTriple = [h, l, r].join("")
-//             if (!ref.has(newTriple)) {
-//                 ref.add(newTriple)
-//                 triplets.push([h, l, r])
-//             }
-//         }
-//         if (left === lLimit && right === rLimit) {
-//             holder++
-//             left = holder + 1
-//             right = holder + 2
-//         } else if (right === rLimit) {
-//             left++
-//             right = left + 1
-//         } else {
-//             right++
+		// If `num[i] === num[i - 1]`, just continue
+		//      Since the array is sorted, any number that is the same as the previous don't need to be checked
+		//      Because the permutations would've have been checked for already the first time
+		if (i > 0 && nums[i] === nums[i - 1]) {
+			continue;
+		}
 
-//         }
-//     }
-//     return triplets
+		// Set a left index to be `i + 1` to avoid using the same number set by the for loop
+		let l = i + 1;
+		// Set a right index to be the last element in the `nums` array
+		let r = nums.length - 1;
+
+		// While `l` pointer is less than `r` pointer
+		while (l < r) {
+			// Get the sum of the current number and the left and right pointer numbers
+			const sum = curr + nums[l] + nums[r];
+
+			// If the current sum is less than 0, then move the left pointer right
+			//      Because nums array is sorted from least to greatest, the only way to get sum higher is to move the lower bound number up
+			if (sum < 0) {
+				l++;
+			}
+			// If the sum is greater than 0, move the right pointer left
+			//      Because nums array is sorted from least to greatest, only way to get sum lower is to move the upper bound number down
+			else if (sum > 0) {
+				r--;
+			}
+			// Otherwise, sum is 0
+			else {
+				// Push to `ans` array an array of `curr, nums[l], num[r]`
+				//      Order is not an issue because array was already sorted
+				ans.push([curr, nums[l], nums[r]]);
+
+				// Move left over to the right
+				l++;
+
+				// While left pointer is less than right pointer and the new number at left pointer is the same as what is was previously, move the left pointer over 1 again
+				//      Keep repeating until the left pointer is looking a unique number
+                //      This is to avoid repeating steps with the same number
+				while (l < r && nums[l] === nums[l - 1]) l++;
+			}
+		}
+	}
+
+	return ans;
+};
+
+// // Brute force solution
+// export const threeSum = (nums) => {
+// 	const ans = [];
+// 	const triplets = new Set();
+
+// 	for (let i = 0; i < nums.length; i++) {
+// 		for (let j = i + 1; j < nums.length; j++) {
+// 			for (let k = j + 1; k < nums.length; k++) {
+// 				const num1 = nums[i];
+// 				const num2 = nums[j];
+// 				const num3 = nums[k];
+
+// 				if (num1 + num2 + num3 === 0) {
+// 					const sort = [num1, num2, num3].sort((a, b) => a - b);
+// 					const joined = sort.join(",");
+
+// 					if (!triplets.has(joined)) {
+// 						ans.push(sort);
+// 						triplets.add(joined);
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return ans;
 // };
 
-export const threeSum = function (nums) {
-    let hold = 0
-    let left = 1
-    let right = 2
-    let triplets = []
-    let ref = new Set()
-
-    while (hold < nums.length - 2) {
-        let h = nums[hold]
-        let l = nums[left]
-        let r = nums[right]
-
-        if (h + l + r === 0) {
-            let check = [h, l, r].join("")
-            if (!ref.has(check)) {
-                ref.add(check)
-                triplets.push([h, l, r])
-            }
-        }
-    }
-}
-
-console.log(threeSum([3, 0, -2, -1, 1, 2])) // [-2, -1, 0, 1, 2, 3]
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); // [ -4, -1, -1, 0, 1, 2 ]
--- tests/15-3-sum.test.js
+++ tests/15-3-sum.test.js
@@ -1,20 +1,27 @@
 import { describe, expect, test } from "vitest";
-import { threeSum } from "../questions/15-3-sum/15";
+import { threeSum } from "../questions/15-3-sum";
 
 describe("15 - 3 Sum", () => {
-    test("Case 1", () => {
-        expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([[-1, -1, 2], [-1, 0, 1]])
-    })
+	test("Case 1", () => {
+		expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
+			[-1, -1, 2],
+			[-1, 0, 1],
+		]);
+	});
 
-    test("Case 2", () => {
-        expect(threeSum([0, 1, 1])).toEqual([])
-    })
+	test("Case 2", () => {
+		expect(threeSum([0, 1, 1])).toEqual([]);
+	});
 
-    test("Case 3", () => {
-        expect(threeSum([0, 0, 0])).toEqual([[0, 0, 0]])
-    })
+	test("Case 3", () => {
+		expect(threeSum([0, 0, 0])).toEqual([[0, 0, 0]]);
+	});
 
-    test("Case 4", () => {
-        expect(threeSum([3, 0, -2, -1, 1, 2])).toEqual([[-2, -1, 3], [-2, 0, 2], [-1, 0, 1]])
-    })
-})
+	test("Case 4", () => {
+		expect(threeSum([3, 0, -2, -1, 1, 2])).toEqual([
+			[-2, -1, 3],
+			[-2, 0, 2],
+			[-1, 0, 1],
+		]);
+	});
+});
