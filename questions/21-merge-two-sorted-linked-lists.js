1b63633cdef7cfcfc990121c3d2cb03476cb12b4
irelius <kihong711@hotmail.com>
2025-10-27 14:46:07 -0700

    safety commit

--- questions/21-merge-two-sorted-linked-lists.js
+++ questions/21-merge-two-sorted-linked-lists.js
@@ -14,40 +14,40 @@ export const mergeTwoSortedLinkedLists = (list1, list2) => {
 	// construct a new ListNode to start the merged list
 	const merged = new ListNode();
 
-    // set `curr` to the head of `merged`, the newly generated linked list
-    //      represents the current node of the linked list
+	// set `curr` to the head of `merged`, the newly generated linked list
+	//      represents the current node of the linked list
 	let curr = merged;
 
-    // While `list1` and `list2` are valid (they are nodes)
+	// While `list1` and `list2` are valid (they are nodes)
 	while (list1 && list2) {
-        // compare values of the current `list1` and `list2` nodes
-        // if `list1` has the smaller value
+		// compare values of the current `list1` and `list2` nodes
+		// if `list1` has the smaller value
 		if (list1.val < list2.val) {
-            // set `curr`'s next pointer to `list1`
+			// set `curr`'s next pointer to `list1`
 			curr.next = list1;
 
-            // update `list1` to be its next node
+			// update `list1` to be its next node
 			list1 = list1.next;
 		}
-        // otherwise, `list2` has the smaller (or equal) value
-        else {
-            // set `curr`'s next pointer to `list2`
+		// otherwise, `list2` has the smaller (or equal) value
+		else {
+			// set `curr`'s next pointer to `list2`
 			curr.next = list2;
 
-            // update `list2` to be its next node
+			// update `list2` to be its next node
 			list2 = list2.next;
 		}
 
-        // change `curr` to be its next node to be able to build out the linked list
+		// change `curr` to be its next node to be able to build out the linked list
 		curr = curr.next;
 	}
 
-    // if `list1` or `list2` is longer than the other, then one will be `null` and the other will still have more nodes
-    //      set `curr`'s next pointer to be the head of the list that still has more nodes
-    //      basically attaches the remaining of the linked list to the end of `curr`
+	// if `list1` or `list2` is longer than the other, then one will be `null` and the other will still have more nodes
+	//      set `curr`'s next pointer to be the head of the list that still has more nodes
+	//      basically attaches the remaining of the linked list to the end of `curr`
 	curr.next = list1 || list2;
 
-    // return `merged.next` because the head value of `merged` is 0
-    //      default value when initializing `merged` at the top
+	// return `merged.next` because the head value of `merged` is 0
+	//      default value when initializing `merged` at the top
 	return merged.next;
 };
